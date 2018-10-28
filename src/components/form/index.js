d235f8bf63ca36b9e866642e1d943d47c7ec0f2d
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2018-10-28 16:36:02 -0300

    Adding a submit handle event on the form

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Form = () => (
+const Form = ({handleSubmite}) => (
 	<div>
-		<form >
-		<label>Product name: </label>
-		<input type="text"/>
-		<input type="submit" value="Submit"/>
-		</form> 
-	</div>
+    <form onSubmit={handleSubmite}>
+        <div className="ui action input">
+            <input type="text" name="formSearch"/>
+            <button className="ui button" >Search</button>
+        </div>
+    </form>
+    </div> 
 )
 
 export default Form
