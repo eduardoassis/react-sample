04ec6333daab2e2e96f9351a17b602593a4ead1b
Eduardo Assis da Silva <eduardouflassis@gmail.com>
2018-10-28 17:18:12 -0300

    Fetching product list from an API and setting on state

--- src/App.js
+++ src/App.js
@@ -9,13 +9,36 @@ class App extends Component {
   }
   
   handleSubmit(e){
-    alert(e.target.formSearch.value)
+    e.preventDefault()
+    this.getApiProducts(e.target.formSearch.value)
+  }
+
+  listProducts(query) {
+
+    const url =`https://api.mercadolibre.com/sites/MLA/search?q=${value}`
+
+    fetch(url)
+    .then((response) => response.json())
+    .then((items) => {
+      this.setState({
+        productList: items.map((item) => ({
+          id: item.id,
+          title: item.title,
+          price: item.price,
+          image: item.thumbnail
+        }))
+      })
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
   }
 
   render() {
     return (
       <div className="App">
-        <AppContainer  handleSubmit={(e) => this.handleSubmit(e)}/>
+        <AppContainer  handleSubmit={(e) => this.handleSubmi(te)}/>
       </div>
     );
   }
